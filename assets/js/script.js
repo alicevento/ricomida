bbe9db91af542a9e998f41ab74651735fcf325da
Alicia Vento <aliciavento@gmail.com>
2024-01-31 19:44:03 -0300

    Iniciando Git en Ricomida

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function () {
+    // Evento "INGREDIENTES"
+    $("#ingredientes").on("click", function () {
+        $(this).css("color", "red");
+        $(this).css("border-bottom", "1px solid red");
+    });
+});
+
+$(document).ready(function () {
+    // Evento "PREPARACION"
+    $("#preparacion").on("click", function () {
+        $(this).css("color", "red");
+        $(this).css("border-bottom", "1px solid red");
+    });
+});
+
+$(".vanish-btn").click(function () {
+  $(".vanish").hide();
+});
+
+//Tooltip
+var tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+var tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
